9e916f8b09a11cc3f9b832ca233c5fe50061da71
zhouzenghui <zhouzenghui.p@qq.com>
2023-02-24 14:04:50 +0800

    对相关的进行更新

--- github_myprofile_updater/update.py
+++ github_myprofile_updater/update.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     # _homepage = open(f'{base_dir}/homepage.md').read().strip()
     _pub = open(f'{base_dir}/pub_short.md').read().strip()
     _news = open(f'{base_dir}/news.md').read().strip()
-    _news = open(f'{base_dir}/others.md').read().strip()
+    _other = open(f'{base_dir}/others.md').read().strip()
     with open('README.md', 'w') as f:
         f.write(_header)
         f.write('\n\n')
